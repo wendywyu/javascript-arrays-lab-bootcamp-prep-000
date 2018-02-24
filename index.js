10d6cee54548300be442cf6e243ee9f36af47c43
wendy yu <yu.wendyw@gmail.com>
2018-02-24 21:54:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = "I don't do much."
 const kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, element) {
-  const moreKittens = [...kittens]
-  array.push()]
+  array.push()
   return array
 }
 
